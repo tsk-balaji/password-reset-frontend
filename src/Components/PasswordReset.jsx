e78482ac82424b4d055981c4d1032549fc9d6e58
Balaji T <tskbalaji15@gmail.com>
2024-11-07 05:35:16 +0530

    Update PasswordReset.jsx

--- src/Components/PasswordReset.jsx
+++ src/Components/PasswordReset.jsx
@@ -1,4 +1,6 @@
 // eslint-disable-next-line no-unused-vars
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { React, useState } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
 import axios from "axios";
